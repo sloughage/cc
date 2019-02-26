5abc7820d094c21c3c65c9bfc750d4d99156762c
sloughage <lanesailer@gmail.com>
2019-02-26 14:28:01 -0600

    git adventure

--- angel/js/lvl/test1.js
+++
@@ -1,5 +0,0 @@
-import Angel from '../obj/Angel.js'
-
-export default {
-  units: [new Angel(200, 200)]
-}
--- etc/test.js
+++
@@ -1,6 +0,0 @@
-import '/class.js'
-
-let j = new hoop()
-console.log(j.i)
-j.add(3)
-console.log(j.i)
